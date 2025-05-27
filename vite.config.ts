93f8d667b2308fe3ed84756c94a7e0eb6263747f
Riuse <riuserin@mail.ru>
2025-05-28 02:59:40 +0300

    final

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import cssnano from 'cssnano'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/tournament_bracket/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
